368c324ae34e2262261d597961260ff2556ede9e
avirozen <117524971+avirozen@users.noreply.github.com>
2022-12-25 18:55:21 +0200

    with sound

--- main.ts
+++ main.ts
@@ -1,10 +1,9 @@
 input.onButtonPressed(Button.A, function () {
     servos.P0.setAngle(0)
+    music.playTone(523, music.beat(BeatFraction.Whole))
 })
 input.onButtonPressed(Button.B, function () {
     servos.P0.setAngle(180)
+    music.playTone(262, music.beat(BeatFraction.Whole))
 })
 servos.P0.setAngle(90)
-basic.forever(function () {
-	
-})
